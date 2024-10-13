c85aea0b30ae2fd80e2dc3f02a6df0f69d75ec0a
sala7khaled <sala7khaled7@gmail.com>
2024-10-13 21:34:47 +0300

    Initial Commit

---
+++ ValifyCaptureDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ValifyCaptureDemo
+//
+//  Created by Salah Khaled on 13/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
